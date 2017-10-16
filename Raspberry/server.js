0ac08f77fe8b90f3bb8c1a20a376451665717380
Valentin Bizeray <valentin@mbp-de-valentin-1.home>
2017-10-16 15:34:49 +0200

    Ajout d'API

--- Raspberry/server.js
+++ Raspberry/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var io = require('socket.io');
+var mysql = require('mysql');
 
 //Variables de test avant la mise en place de la base de données
 const listLamp = ["Cuisine", "Salle à Manger", "Chambre"];
@@ -18,9 +19,10 @@ var server = http.createServer(function (req, res) {
 WSServer = io.listen(server);
 
 
-// Quand un client se connecte, on le note dans la console
+
 WSServer.sockets.on('connection', function (socket) {
-    socket.broadcast.emit("message", "zebfiezoybo");
+
+    // Quand un client se connecte, on le note dans la console
     console.log('Un client est connecté !');
 
     socket.on("getListLamp", msg => {
@@ -33,13 +35,38 @@ WSServer.sockets.on('connection', function (socket) {
         socket.emit("listLamp", { list: ["Cuisine", "Salle à Manger", "Chambre"], state: state });
     });
 
+    //Réaction à la requête de changement d'état d'une lampe
     socket.on("changeLampState", msg => {
 
         var stateIndex = listLamp.indexOf(msg.location);
         state[stateIndex] = msg.state;
+
+        //Normalement lance un programme mais dans ce cas pour les tests renvoie un message de notification
         socket.broadcast.emit("lampStateChanged", { location: msg.location, state: state[stateIndex], brightness: 50 });
         socket.emit("lampStateChanged", { location: msg.location, state: state[stateIndex], brightness: 50 });
     });
+
+    socket.on("login", msg =>{
+        
+        /*
+            Recherche et vérification dans la base de données pour l'authentification
+        */
+
+        //Envoi d'un message template indicant un succès de connexion
+        socket.emit("loginResult", {success: true});
+        //socket.emit("loginResult", {success: false});
+
+    });
+
+
+    /*En cas de perte dela communication avec le Arduino :
+        PS : Préciser la localisation de la lampe grâce à la BDD
+        socket.emit("zigbeeFail", {error: "Erreur : la connexion avec la lampe à été perdu"});
+
+        En cas de défaillance du module zigbee sur la Raspberry
+        socket.emit("zigbeeFail", {error: "Erreur : Le module Zigbee de la Raspberry est défaillant});
+    */
+    
 
 });
 
