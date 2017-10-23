80d242b708b6fab7bba23430ed1afce55f4e5c2a
Valentin Bizeray <valentin@mbp-de-valentin.home>
2017-10-24 00:21:55 +0200

    Ajout de l'interraction avec la BDD

--- Raspberry/server.js
+++ Raspberry/server.js
@@ -1,5 +1,4 @@
-//var http = require('http');
-var https = require('https');
+var http = require('http');
 var fs = require('fs');
 var io = require('socket.io');
 var mysql = require('mysql');
@@ -8,20 +7,34 @@ var mysql = require('mysql');
 const listLamp = ["Cuisine", "Salle à Manger", "Chambre"];
 var state = [true, false, true];
 
-// Chargement du fichier index.html affiché au client
-var server = https.createServer({
-    key: fs.readFileSync('encryption/key.pem'),
-    cert: fs.readFileSync('encryption/cert.pem')
-}, function (req, res) {
-    fs.readFile('./test.html', 'utf-8', function (error, content) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
-    });
+
+var mysqlClient = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'PX504',
+    database: 'smart_remote'
 });
 
-// Chargement de socket.io
+
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function (req, res) {
+        fs.readFile('./test.html', 'utf-8', function (error, content) {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content);
+        });
+    });
+
+server.on('error', error => {
+    console.log(error);
+    process.exit();
+});
+
+
+
 WSServer = io.listen(server);
 
+// Chargement de socket.io
 
 
 WSServer.sockets.on('connection', function (socket) {
@@ -31,39 +44,50 @@ WSServer.sockets.on('connection', function (socket) {
 
     socket.on("getListLamp", msg => {
 
-        /*
-            Partie où l'on recherche les lampes sur la BDD
-        */
-
-        //Template de liste pour tester la fonctionnalité du message
-        socket.emit("listLamp", { list: ["Cuisine", "Salle à Manger", "Chambre"], state: state });
+        mysqlClient.query("SELECT * FROM Lamp INNER JOIN Control ON Lamp.id = Control.id WHERE Control.username = ?", [msg.user], (error, result) => {
+            socket.emit('listLamp', { list: result });
+        });
     });
 
     //Réaction à la requête de changement d'état d'une lampe
     socket.on("changeLampState", msg => {
 
-        var stateIndex = listLamp.indexOf(msg.location);
-        state[stateIndex] = msg.state;
 
-        //Normalement lance un programme mais dans ce cas pour les tests renvoie un message de notification
-        socket.broadcast.emit("lampStateChanged", { location: msg.location, state: state[stateIndex], brightness: 50 });
-        socket.emit("lampStateChanged", { location: msg.location, state: state[stateIndex], brightness: 50 });
+
+        mysqlClient.query(
+            "UPDATE Lamp SET State = ?, brightness = ? WHERE location = ?",
+            [msg.state, msg.brightness, msg.location],
+            (error, result) => {
+                if (error != null) {
+                    console.log(error);
+                } else {
+                    socket.broadcast.emit("lampStateChanged", { location: msg.location, state: msg.state, brightness: msg.brightness });
+                    socket.emit("lampStateChanged", { location: msg.location, state: msg.state, brightness: msg.brightness });
+                }
+            });
+
     });
 
     socket.on("login", msg => {
 
-        /*
-            Recherche et vérification dans la base de données pour l'authentification
-        */
+        //Recherche et vérification dans la base de données pour l'authentification
 
-        //Envoi d'un message template indicant un succès de connexion
-        socket.emit("loginResult", { success: true });
-        //socket.emit("loginResult", {success: false});
+        mysqlClient.query("SELECT password FROM User WHERE username=?", [msg.user], (error, result) => {
 
+            if (error != null) {
+                socket.emit("loginResult", { success: false });
+                console.log(error);
+            } else {
+                if (result[0].password == msg.password) {
+                    socket.emit("loginResult", { success: true });
+                }
+            }
+        });
     });
 
 
     /*En cas de perte dela communication avec le Arduino :
+    
         PS : Préciser la localisation de la lampe grâce à la BDD
         socket.emit("zigbeeFail", {error: "Erreur : la connexion avec la lampe à été perdu"});
 
@@ -76,7 +100,14 @@ WSServer.sockets.on('connection', function (socket) {
 
 
 
-
-server.listen(8080);
-
-console.log("Serveur démarré, pour vous connecter à celui-ci, dans un navigateur Web, entrez l'adresse https://localhost:8080");
+mysqlClient.connect(error => {
+    if (error != null) {
+        console.log(error);
+        process.exit();
+    }
+    else {
+        console.log("Connexion à la base de données réussi");
+        server.listen(8080);
+        console.log("Serveur démarré, pour vous connecter à celui-ci, dans un navigateur Web, entrez l'adresse https://localhost:8080");
+    }
+});
