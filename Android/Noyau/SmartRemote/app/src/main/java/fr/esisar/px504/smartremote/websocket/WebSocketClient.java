fd95b098bdda6828234f73a4937379416a1db5b0
Valentin <valentin@esisar.px>
2017-11-13 13:01:59 +0100

    Modifications du serveur

--- Android/Noyau/SmartRemote/app/src/main/java/fr/esisar/px504/smartremote/websocket/WebSocketClient.java
+++ Android/Noyau/SmartRemote/app/src/main/java/fr/esisar/px504/smartremote/websocket/WebSocketClient.java
@@ -28,7 +28,7 @@ public class WebSocketClient {
     public static final int STATE_CHANGED = 2;
     public static final int LOGIN_RESULT = 3;
     public static final int ERROR = 4;
-    public static final String WS_SERVER = "http://192.168.20.1:8080";
+    public static final String WS_SERVER = "http://192.168.10.1:8080";
     final private WebSocketHandler webSocketHandler;
 
     /**
--- Raspberry/finalServer.js
+++ Raspberry/finalServer.js
@@ -4,14 +4,14 @@ var io = require('socket.io');
 var mysql = require('mysql');
 const EventEmitter = require("events");
 var SerialPort = require("serialport");
-var port = new SerialPort("/dev/ttyAMA0", {baudRate: 9600});
+var port = new SerialPort("/dev/ttyAMA0", { baudRate: 9600 });
 var count = 0;
 
-class MyEmitter extends EventEmitter{};
+class MyEmitter extends EventEmitter { };
 
 const myEmitter = new MyEmitter();
 
-myEmitter.on("ok", function() {
+myEmitter.on("ok", function () {
 
 
     mysqlClient.connect(error => {
@@ -29,141 +29,159 @@ myEmitter.on("ok", function() {
 });
 
 var mysqlClient = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'PX504',
-        database: 'smart_remote'
+    host: 'localhost',
+    user: 'root',
+    password: 'PX504',
+    database: 'smart_remote'
+});
+
+
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function (req, res) {
+    fs.readFile('./test.html', 'utf-8', function (error, content) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+    });
+});
+
+server.on('error', error => {
+    console.log(error);
+    process.exit();
+});
+
+
+
+WSServer = io.listen(server);
+
+// Chargement de socket.io
+
+
+WSServer.sockets.on('connection', function (socket) {
+
+    // Quand un client se connecte, on le note dans la console
+    console.log('Un client est connecté !');
+
+    socket.join('lampRoom');
+
+    socket.on("getListLamp", msg => {
+
+        console.log('Demande liste de lampe par ' + msg.user);
+
+        mysqlClient.query("SELECT * FROM Lamp INNER JOIN Control ON Lamp.id = Control.id WHERE Control.username = ?", [msg.user], (error, result) => {
+            socket.emit('getListLampResult', { list: result });
+        });
     });
 
+    //Réaction à la requête de changement d'état d'une lampe
+    socket.on("setLamp", msg => {
 
+        console.log('Requète de changement');
 
-    // Chargement du fichier index.html affiché au client
-    var server = http.createServer(function (req, res) {
-            fs.readFile('./test.html', 'utf-8', function (error, content) {
-                res.writeHead(200, { "Content-Type": "text/html" });
-                res.end(content);
-            });
-        });
-
-    server.on('error', error => {
-        console.log(error);
-        process.exit();
-    });
-
-
-
-    WSServer = io.listen(server);
-
-    // Chargement de socket.io
-
-
-    WSServer.sockets.on('connection', function (socket) {
-
-        // Quand un client se connecte, on le note dans la console
-        console.log('Un client est connecté !');
-
-        socket.join('lampRoom');
-
-        socket.on("getListLamp", msg => {
-
-            console.log('Demande liste de lampe par '+msg.user);
-
-            mysqlClient.query("SELECT * FROM Lamp INNER JOIN Control ON Lamp.id = Control.id WHERE Control.username = ?", [msg.user], (error, result) => {
-                socket.emit('getListLampResult', { list: result });
-            });
-        });
-
-        //Réaction à la requête de changement d'état d'une lampe
-        socket.on("setLamp", msg => {
-
-            console.log('Requète de changement');
-
-            /*mysqlClient.query(
-                "UPDATE Lamp SET State = ?, brightness = ? WHERE location = ?",
-                [msg.state, msg.brightness, msg.location],
-                (error, result) => {
-                    if (error != null) {
-                        console.log(error);
-                    } else {
-                        socket.broadcast.emit("setLampResult", { location: msg.location, state: msg.state, brightness: msg.brightness });
-                        socket.emit("setLampResult", { location: msg.location, state: msg.state, brightness: msg.brightness });
-                    }
-                });*/
-
-                mysqlClient.query("SELECT * FROM Lamp WHERE location=?", [msg.location], (error, result) => {
-                    setLamp(result[0].id, String(msg.brightness));
-                });
-
-                
-
-        });
-
-        socket.on("login", msg => {
-
-            console.log('Identification');
-
-            //Recherche et vérification dans la base de données pour l'authentification
-
-            mysqlClient.query("SELECT password FROM User WHERE username=?", [msg.user], (error, result) => {
-
+        /*mysqlClient.query(
+            "UPDATE Lamp SET State = ?, brightness = ? WHERE location = ?",
+            [msg.state, msg.brightness, msg.location],
+            (error, result) => {
                 if (error != null) {
-                    socket.emit("loginResult", { success: false });
                     console.log(error);
                 } else {
-                    if(result[0] != null) {
-                        if (result[0].password == msg.password) {
-                            socket.emit("loginResult", { success: true });
-                        } else {
-                            socket.emit("loginResult", { success: false });
-                        }
+                    socket.broadcast.emit("setLampResult", { location: msg.location, state: msg.state, brightness: msg.brightness });
+                    socket.emit("setLampResult", { location: msg.location, state: msg.state, brightness: msg.brightness });
+                }
+            });*/
+
+        mysqlClient.query("SELECT * FROM Lamp WHERE location=?", [msg.location], (error, result) => {
+            if (msg.state == "off") {
+                setLamp(result[0].id, String(0));
+            } else {
+                setLamp(result[0].id, String(msg.brightness));
+            }
+
+        });
+
+
+
+    });
+
+    socket.on("login", msg => {
+
+        console.log('Identification');
+
+        //Recherche et vérification dans la base de données pour l'authentification
+
+        mysqlClient.query("SELECT password FROM User WHERE username=?", [msg.user], (error, result) => {
+
+            if (error != null) {
+                socket.emit("loginResult", { success: false });
+                console.log(error);
+            } else {
+                if (result[0] != null) {
+                    if (result[0].password == msg.password) {
+                        socket.emit("loginResult", { success: true });
                     } else {
                         socket.emit("loginResult", { success: false });
                     }
-                    
+                } else {
+                    socket.emit("loginResult", { success: false });
                 }
-            });
+
+            }
         });
-
-
-        /*En cas de perte dela communication avec le Arduino :
-        
-            PS : Préciser la localisation de la lampe grâce à la BDD
-            socket.emit("zigbeeFail", {error: "Erreur : la connexion avec la lampe à été perdu"});
-
-            En cas de défaillance du module zigbee sur la Raspberry
-            socket.emit("zigbeeFail", {error: "Erreur : Le module Zigbee de la Raspberry est défaillant});
-        */
-
-
     });
 
-port.on("open", function(){
+
+    /*En cas de perte dela communication avec le Arduino :
+    
+        PS : Préciser la localisation de la lampe grâce à la BDD
+        socket.emit("zigbeeFail", {error: "Erreur : la connexion avec la lampe à été perdu"});
+
+        En cas de défaillance du module zigbee sur la Raspberry
+        socket.emit("zigbeeFail", {error: "Erreur : Le module Zigbee de la Raspberry est défaillant});
+    */
+
+
+});
+
+port.on("open", function () {
     console.log("open");
     xbeeInit();
-    port.on("data", function(data){
+    port.on("data", function (data) {
         console.log("data received: " + data);
-        if(data == "OK\r"){
+        if (data == "OK\r") {
             xbeeInit();
         } else {
 
             var msg = parseData(data);
-            if(msg.brightness == 0) {
+            if (msg.brightness == 0) {
+
+                mysqlClient.query(
+                    "UPDATE Lamp SET State = ? WHERE location = ?",
+                    ["off", msg.location],
+                    (error, result) => {
+                        if (error != null) {
+                            console.log(error);
+                        } else {
+                            //socket.broadcast.emit("setLampResult", { location: msg.location, state: msg.state, brightness: msg.brightness });
+                            //socket.emit("setLampResult", { location: msg.location, state: msg.state, brightness: msg.brightness });
+                            socket.to('lampRoom').emit("setLampResult", { location: msg.location, state: msg.state, brightness: msg.brightness });
+                        }
+                    });
 
             } else {
                 mysqlClient.query(
-                "UPDATE Lamp SET State = ?, brightness = ? WHERE location = ?",
-                ["on", msg.brightness, msg.location],
-                (error, result) => {
-                    if (error != null) {
-                        console.log(error);
-                    } else {
-                        //socket.broadcast.emit("setLampResult", { location: msg.location, state: msg.state, brightness: msg.brightness });
-                        //socket.emit("setLampResult", { location: msg.location, state: msg.state, brightness: msg.brightness });
-                        socket.to('lampRoom').emit("setLampResult", { location: msg.location, state: msg.state, brightness: msg.brightness });
-                    }
-            });
+                    "UPDATE Lamp SET State = ?, brightness = ? WHERE location = ?",
+                    ["on", msg.brightness, msg.location],
+                    (error, result) => {
+                        if (error != null) {
+                            console.log(error);
+                        } else {
+                            //socket.broadcast.emit("setLampResult", { location: msg.location, state: msg.state, brightness: msg.brightness });
+                            //socket.emit("setLampResult", { location: msg.location, state: msg.state, brightness: msg.brightness });
+                            socket.to('lampRoom').emit("setLampResult", { location: msg.location, state: msg.state, brightness: msg.brightness });
+                        }
+                    });
             }
-            
+
         }
     });
 
@@ -172,14 +190,14 @@ port.on("open", function(){
 //xbeeInit(count);
 
 
-function setLamp(id, data){
-    port.write(id + "\r" + data +" ");
+function setLamp(id, data) {
+    port.write(id + "\r" + data + " ");
 };
 
-function xbeeInit(){
+function xbeeInit() {
 
-    switch(count) {
-        case 0: 
+    switch (count) {
+        case 0:
             console.log("Xbee Init");
             port.write("+++");
             console.log("+++");
@@ -216,6 +234,6 @@ function parseData(data) {
     var id = data.substr(0, data.indexOf("\r"));
     var brightness = data.substr(data.indexOf("\r"), data.indexOf(" "));
     mysqlClient.query("SELECT * FROM Lamp WHERE id=?", [parseInt(id)], (error, result) => {
-        return({location: result[0].location, brightness: parseInt(brightness)});
+        return ({ location: result[0].location, brightness: parseInt(brightness) });
     });
 }
